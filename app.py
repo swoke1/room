b6994c80298500cc6ad26b2c9908d3ef7389ebfe
swoke1 <sulbol4ik@gmail.com>
2026-05-23 14:29:49 +0300

    app.py

--- app.py
+++ app.py
@@ -527,3 +527,11 @@ def chat():
     </script>
 
     """
+if __name__ == "__main__":
+
+    port = int(os.environ.get("PORT", 8000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
