3bf6544d72a424ccc9a2573389f8bb807d14bcdb
swoke1 <sulbol4ik@gmail.com>
2026-05-23 14:48:36 +0300

    app.py

--- app.py
+++ app.py
@@ -558,9 +558,7 @@ def logout():
 # ---------- RUN ----------
 if __name__ == "__main__":
 
-    port = int(os.environ.get("PORT", 8000))
-
     app.run(
         host="0.0.0.0",
-        port=port
+        port=int(os.environ.get("PORT", 8080))
     )
